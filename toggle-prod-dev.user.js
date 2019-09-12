95812fdb9caa66f5c12812270e9699bdbbc0a0cf
Roy van de Water <roy.vwater@gmail.com>
2019-09-12 13:50:48 -0700

    Update Toggle prod/dev to use app.circlehost for dev

--- toggle-prod-dev.user.js
+++ toggle-prod-dev.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Toggle Prod/Dev
 // @namespace    http://royvandewater.com/
-// @version      1.0
-// @updateURL    https://gist.github.com/royvandewater/04d024bf19c88cb1e3a77d067269d86f/raw/toggle-prod.user.js
+// @version      1.1
+// @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/toggle-prod-dev.user.js
 // @description  Toggle between production & development environments
 // @author       You
 // @match        https://app.circleci.com/*
-// @match        http://localhost:3000/*
+// @match        http://app.circlehost:3000/*
 // @grant        none
 // @require      https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
 // ==/UserScript==
@@ -15,11 +15,11 @@
     'use strict';
 
     window.hotkeys('command+k', () => {
-        if (window.location.host.startsWith("localhost")) {
+        if (window.location.host.startsWith("app.circlehost")) {
             window.location = "https://app.circleci.com" + window.location.pathname
             return
         }
 
-        window.location = "http://localhost:3000" + window.location.pathname
+        window.location = "http://app.circlehost:3000" + window.location.pathname
     })
 })();
