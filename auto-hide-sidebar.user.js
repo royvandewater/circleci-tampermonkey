eb67c5a6573ecb4bfd0c2af1e89a8fa559e52cb3
Roy van de Water <roy.vwater@gmail.com>
2019-09-13 11:04:03 -0700

    Fix sidebar matcher

--- auto-hide-sidebar.user.js
+++ auto-hide-sidebar.user.js
@@ -2,7 +2,7 @@
 // @name         Auto Close Side Panel
 // @namespace    http://royvandewater.com/
 // @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/auto-hide-sidebar.user.js
-// @version      1.3
+// @version      1.4
 // @description  Automatically close the side panel on initial page load
 // @author       Roy van de Water
 // @match        https://app.circleci.com/jobs/*
@@ -13,18 +13,20 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+  "use strict";
 
-    const closeButtonMatcher = '[title="Close Details Pane"]';
-    const openButtonMatcher = '[title="Open Details Pane"]';
+  const closeButtonMatcher = '[aria-label="Close Details Pane"]';
+  const openButtonMatcher = '[aria-label="Open Details Pane"]';
 
-    let active = true;
+  let active = true;
 
-    const click = element => {
-        if (!active) return;
-        element.click();
-    }
+  const click = element => {
+    if (!active) return;
+    element.click();
+  };
 
-    document.arrive(closeButtonMatcher, click);
-    document.arrive(openButtonMatcher, () => { active = false });
+  document.arrive(closeButtonMatcher, click);
+  document.arrive(openButtonMatcher, () => {
+    active = false;
+  });
 })();
