9d9f9b7c54a73ab1c81d76a78174856cc3fd516e
Roy van de Water <roy.vwater@gmail.com>
2019-09-13 11:02:52 -0700

    Add auto-hide sidebar

---
+++ auto-hide-sidebar.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Auto Close Side Panel
+// @namespace    http://royvandewater.com/
+// @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/auto-hide-sidebar.user.js
+// @version      1.3
+// @description  Automatically close the side panel on initial page load
+// @author       Roy van de Water
+// @match        https://app.circleci.com/jobs/*
+// @match        http://localhost:3000/jobs/*
+// @match        http://app.circlehost:3000/jobs/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const closeButtonMatcher = '[title="Close Details Pane"]';
+    const openButtonMatcher = '[title="Open Details Pane"]';
+
+    let active = true;
+
+    const click = element => {
+        if (!active) return;
+        element.click();
+    }
+
+    document.arrive(closeButtonMatcher, click);
+    document.arrive(openButtonMatcher, () => { active = false });
+})();
