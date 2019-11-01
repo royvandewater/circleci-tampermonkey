7797653f408bd07b40c39850c18683cb0a35e744
Roy van de Water <roy.vwater@gmail.com>
2019-11-01 09:46:10 -0700

    Implement without arrive.js

--- auto-collapse-side-navigation.user.js
+++ auto-collapse-side-navigation.user.js
@@ -2,14 +2,13 @@
 // @name         Auto Collapse Primary Navigation
 // @namespace    http://royvandewater.com/
 // @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/auto-collapse-side-navigation.user.js
-// @version      2.1
+// @version      2.2
 // @description  Automatically close the primary navigation on initial page load
 // @author       Roy van de Water
 // @match        https://app.circleci.com/*
 // @match        http://localhost:3000/*
 // @match        http://app.circlehost:3000/*
 // @match        https://circleci.jp/*
-// @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
 // @grant        none
 // ==/UserScript==
 //
@@ -18,11 +17,18 @@
 (function() {
   "use strict";
 
-  const navigationExpanded = () => document.querySelector("nav").clientWidth > 100;
+  const getButton = () => document.querySelector('[aria-label="Toggle Primary Navigation Panel"]')
+  const navigationExpanded = () => {
+    const nav = document.querySelector("nav");
+    return nav && nav.clientWidth > 100;
+  }
 
-  document.arrive('[aria-label="Toggle Primary Navigation Panel"]', button => {
-    if (navigationExpanded()) {
-      button.click();
-    }
-  });
+  const autoCollapse = () => {
+    const button = getButton();
+    if (!button) return;
+    if (!navigationExpanded()) return;
+
+    button.click();
+  }
+  setInterval(autoCollapse, 1000);
 })();
