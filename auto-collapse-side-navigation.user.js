c25d3d0af9241853cce4c257b49ad0a7b12b3ee7
Roy van de Water <roy.vwater@gmail.com>
2019-11-01 08:10:17 -0700

    Also match circleci.jp

--- auto-collapse-side-navigation.user.js
+++ auto-collapse-side-navigation.user.js
@@ -2,12 +2,13 @@
 // @name         Auto Collapse Primary Navigation
 // @namespace    http://royvandewater.com/
 // @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/auto-collapse-side-navigation.user.js
-// @version      1.1
+// @version      1.2
 // @description  Automatically close the primary navigation on initial page load
 // @author       Roy van de Water
 // @match        https://app.circleci.com/*
 // @match        http://localhost:3000/*
 // @match        http://app.circlehost:3000/*
+// @match        https://circleci.jp/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
 // @grant        none
 // ==/UserScript==
