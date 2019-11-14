89d42b3ddc2aaaa96b25eb3d09dcd78638d4a11b
Roy van de Water <roy.vwater@gmail.com>
2019-11-14 16:24:00 -0700

    Match circleci.jp correctly

--- remove-_blank.user.js
+++ remove-_blank.user.js
@@ -2,12 +2,12 @@
 // @name         Remove _blank
 // @namespace    http://royvandewater.com/
 // @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/remove-_blank.user.js
-// @version      1.1
+// @version      1.2
 // @description  Remove all target="_blank" statements to links act like links
 // @author       Roy van de Water
 // @match        https://app.circleci.com/*
 // @match        http://localhost:3000/*
-// @match        https://app.circleci.jp/*
+// @match        https://circleci.jp/*
 // @match        http://app.circlehost:3000/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
 // @grant        none
