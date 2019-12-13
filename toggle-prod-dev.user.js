ecd029b85574cd4443d8eac59670d76136400124
Roy van de Water <roy.vwater@gmail.com>
2019-12-13 14:44:50 -0700

    Add support for org settings

--- toggle-prod-dev.user.js
+++ toggle-prod-dev.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Toggle Prod/Dev
 // @namespace    http://royvandewater.com/
-// @version      3.1
+// @version      3.2
 // @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/toggle-prod-dev.user.js
 // @description  Toggle between production & development environments
 // @author       You
 // @match        https://app.circleci.com/*
+// @match        https://ui.circleci.com/*
 // @match        https://circleci.jp/*
 // @match        http://localhost:3000/*
 // @match        http://app.circlehost:3000/*
@@ -14,14 +15,22 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+  "use strict";
 
-    window.hotkeys('command+k', () => {
-        if (window.location.host.startsWith("app.circleci.com")) {
-            window.location = "http://app.circlehost:3000" + window.location.pathname + window.location.search
-            return
-        }
+  window.hotkeys("command+k", () => {
+    const { host, pathname, search } = window.location;
+    if (
+      host.startsWith("app.circleci.com") ||
+      host.startsWith("ui.circleci.com")
+    ) {
+      window.location = "http://app.circlehost:3000" + pathname + search;
+      return;
+    }
 
-        window.location = "https://app.circleci.com" + window.location.pathname + window.location.search
-    })
+    if (pathname.startsWith("/settings/organization")) {
+      window.location = "https://ui.circleci.com" + pathname + search;
+      return;
+    }
+    window.location = "https://app.circleci.com" + pathname + search;
+  });
 })();
