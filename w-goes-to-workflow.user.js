cfb7671c1bf493f47b13aaaf255ac35e8cb76dbe
Roy van de Water <roy.vwater@gmail.com>
2020-01-09 08:39:41 -0700

    Register script on all web-ui

--- w-goes-to-workflow.user.js
+++ w-goes-to-workflow.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         W goes to workflow
 // @namespace    http://royvandewater.com/
-// @version      1.0
+// @version      1.1
 // @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/w-goes-to-workflow.user.js
 // @description  Will navigate to the workflow page when looking at the job page
 // @author       Roy van de Water
-// @match        https://app.circleci.com/jobs/*
-// @match        https://circleci.jp/jobs/*
-// @match        http://localhost:3000/jobs/*
-// @match        http://app.circlehost:3000/jobs/*
+// @match        https://app.circleci.com/*
+// @match        https://circleci.jp/*
+// @match        http://localhost:3000/*
+// @match        http://app.circlehost:3000/*
 // @grant        none
 // @require      https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
 // @require      https://code.jquery.com/jquery-3.4.1.js
@@ -26,6 +26,8 @@
   };
 
   window.hotkeys("w", () => {
+    if (!window.location.pathname.startsWith("/jobs")) return;
+
     const a = getWorkflowA();
     if (!a) {
       console.warn("Could not find workflow link");
