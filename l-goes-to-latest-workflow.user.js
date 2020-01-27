b805120cb008cf34efdb941dd43281a40d5e66fd
Roy van de Water <roy.vwater@gmail.com>
2020-01-27 14:26:05 -0700

    Make latest workflow finder wait for an new workflow

--- l-goes-to-latest-workflow.user.js
+++ l-goes-to-latest-workflow.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         L goes to latest workflow
 // @namespace    http://royvandewater.com/
-// @version      1.1
+// @version      1.2
 // @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/l-goes-to-latest-workflow.user.js
 // @description  Will navigate to the the latest workflow page of the current branch when looking at the workflow page
 // @author       Roy van de Water
@@ -11,35 +11,37 @@
 // @match        http://app.circlehost:3000/*
 // @grant        none
 // @require      https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
-// @require      https://code.jquery.com/jquery-3.4.1.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
 // ==/UserScript==
 
 (function() {
   "use strict";
-  const $ = window.jQuery;
-  $.noConflict(true);
 
   const branchASelector =
     "#__next > div > div > div > div > div > div > div > a";
   const workflowASelector =
-    "#__next > div > main > div > div > div:nth-child(2) > div:nth-child(4) > a";
+    "#__next > div > main > div > div > div:nth-child(2) > div:nth-child(3) > a";
 
-  const getBranchA = () => {
-    return $(branchASelector)[0];
-  };
+  const getBranchA = async () => (
+    new Promise(resolve => {
+      const a = document.querySelector(branchASelector);
 
-  window.hotkeys("l", () => {
+      if (a) return resolve(a);
+
+      window.arrive(branchASelector, resolve);
+    }),
+  );
+
+  window.hotkeys("l", async () => {
     if (!window.location.pathname.includes("/workflows/")) return;
 
-    const a = getBranchA();
-    if (!a) {
-      console.warn("Could not find branch link");
-      return;
-    }
+    const workflowId = window.location.pathname.split('/').pop();
+
+    const a = await getBranchA();
     a.click();
 
     const click = el => {
+      if (el.href.includes(workflowId)) return;
       document.unbindArrive(click);
       el.click();
     };
