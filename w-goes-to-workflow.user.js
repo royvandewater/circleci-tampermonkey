1616ce7bbd63a2a7dbe0d96c63873a5aff6f51d0
Roy van de Water <roy.vwater@gmail.com>
2020-01-27 14:15:01 -0700

    Fix w-goes-to-workflow

--- w-goes-to-workflow.user.js
+++ w-goes-to-workflow.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         W goes to workflow
 // @namespace    http://royvandewater.com/
-// @version      1.2
+// @version      1.3
 // @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/w-goes-to-workflow.user.js
 // @description  Will navigate to the workflow page when looking at the job page
 // @author       Roy van de Water
@@ -11,24 +11,25 @@
 // @match        http://app.circlehost:3000/*
 // @grant        none
 // @require      https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
-// @require      https://code.jquery.com/jquery-3.4.1.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
 // ==/UserScript==
 
 (function() {
   "use strict";
 
-  const $ = window.jQuery;
-  $.noConflict(true);
+  const workflowSelector = 'ol > li:nth-of-type(2) > a';
 
   window.hotkeys("w", () => {
     if (!window.location.pathname.startsWith("/jobs")) return;
+
+    const a = document.querySelector(workflowSelector);
+    if (a) return a.click();
 
     const click = el => {
       $(document).unbindArrive(click);
       el.click();
     };
 
-    $(document).arrive("ol > li > a:contains(Pipelines)", click);
+    window.arrive(workflowSelector, click);
   });
 })();
