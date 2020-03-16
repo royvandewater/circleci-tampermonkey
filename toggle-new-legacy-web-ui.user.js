14e124c40123587151e76e284a19ae627728b294
Roy van de Water <roy.vwater@gmail.com>
2020-03-16 15:46:04 -0700

    Version 2 of the toggle old/new

--- toggle-new-legacy-web-ui.user.js
+++ toggle-new-legacy-web-ui.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Toggle New/Legacy web-ui
 // @namespace    http://royvandewater.com/
-// @version      1.4
+// @version      2.0
 // @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/toggle-new-legacy-web-ui.user.js
 // @description  Toggle between the New & Legacy web-ui using CMD+J
 // @author       Roy van de Water
@@ -11,67 +11,79 @@
 // @require      https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
 // ==/UserScript==
 
-const oldToNewVcs = { gh: "github", bb: "bitbucket" };
-const newToOldVcs = { github: "gh", bitbucket: "bb" };
-const newWorkflowRegexp = /\/(github|bitbucket)\/[\w-]+\/[\w-]+\/pipelines\/[\w-]+\/workflows\/[\w-]+$/;
-
-const toNewJobPath = oldPath => {
-  const [_, oldVcs, org, repo, job] = oldPath.split("/");
-  const vcs = oldToNewVcs[oldVcs];
-  return `/jobs/${vcs}/${org}/${repo}/${job}`;
-};
-
-const toNewPath = oldPath => {
-  return toNewJobPath(oldPath);
-};
-
-const toOldJobPath = newPath => {
-  const [_, _jobs, newVcs, org, repo, job] = newPath.split("/");
-  const vcs = newToOldVcs[newVcs];
-  return `/${vcs}/${org}/${repo}/${job}`;
-};
-
-const toOldWorkflowsPath = newPath => {
-  const [_, newVcs, org, repo] = newPath.split("/");
-  const vcs = newToOldVcs[newVcs];
-  return `/${vcs}/${org}/workflows/${repo}`;
-};
-
-const toOldWorkflowPath = newPath => {
-  const parts = newPath.split("/");
-  const workflowId = parts[parts.length - 1];
-  return `/workflow-run/${workflowId}`;
-};
-
-const toOldPath = newPath => {
-  if (newPath.startsWith("/jobs")) {
-    return toOldJobPath(newPath);
-  }
-
-  // /github/circleci/web-ui/pipelines
-  if (newPath.startsWith("/github") && newPath.endsWith("/pipelines")) {
-    return toOldWorkflowsPath(newPath);
-  }
-
-  // /github/circleci/web-ui/pipelines/<uuid>/workflows/<uuid>
-  if (newWorkflowRegexp.test(newPath)) {
-    return toOldWorkflowPath(newPath);
-  }
-
-  console.error("Unknown path, refusing to do anything");
-};
 
 (function() {
   "use strict";
 
-  window.hotkeys("command+j", () => {
+  const oldToNewVcs = { gh: "github", bb: "bitbucket" };
+  const newToOldVcs = { github: "gh", bitbucket: "bb" };
+
+  const newToOldMappings = [{
+    name: 'Pipelines',
+    match: new RegExp('^/pipelines/([\\w-]+)/([\\w-]+)/([\\w-]+)$'),
+    convert: ([_, vcsType, orgName, projectName]) => `/${newToOldVcs[vcsType]}/${orgName}/workflows/${projectName}`,
+  }, {
+    name: 'Workflows',
+    match: new RegExp('^/pipelines/[\\w-]+/[\\w-]+/[\\w-]+/\\d+/workflows/([\\w-]+)$'),
+    convert: ([_, workflowId]) => `/workflow-run/${workflowId}`,
+  }, {
+    name: 'Job',
+    match: new RegExp('^/pipelines/([\\w-]+)/([\\w-]+)/([\\w-]+)/\\d+/workflows/[\\w-]+/jobs/(\\d+)$'),
+    convert: ([_, vcsType, orgName, projectName, jobNumber]) => `/${newToOldVcs[vcsType]}/${orgName}/${projectName}/${jobNumber}`,
+  }]
+
+  const oldToNewMappings = [{
+    name: 'Job',
+    match: new RegExp('^/([\\w-]+)/([\\w-]+)/([\\w-]+)/(\\d+)'),
+    convert: ([_, vcsType, orgName, projectName, jobNumber]) => `/jobs/${oldToNewVcs[vcsType]}/${orgName}/${projectName}/${jobNumber}`,
+  }, {
+    name: 'Pipelines',
+    match: new RegExp('^/([\\w-]+)/([\\w-]+)/workflows/([\\w-]+)$'),
+    convert: ([_, vcsType, orgName, projectName]) => `/pipelines/${oldToNewVcs[vcsType]}/${orgName}/${projectName}`,
+  }]
+
+  const toOldPath = newPath => {
+    const mapping = newToOldMappings.find(({match}) => match.test(newPath))
+    if (!mapping) return
+
+    return mapping.convert(mapping.match.exec(newPath))
+  };
+
+  const toNewPath = oldPath => {
+    const mapping = oldToNewMappings.find(({match}) => match.test(oldPath))
+    if (!mapping) return
+
+    return mapping.convert(mapping.match.exec(oldPath))
+  }
+
+  const getNewLocation = () => {
+    const newPathname = toNewPath(window.location.pathname)
+    if (!newPathname) return null ;
+
+    return new URL(newPathname, 'https://app.circleci.com').toString()
+  }
+
+  const getOldLocation = () => {
+    const oldPathname = toOldPath(window.location.pathname)
+    if (!oldPathname) return;
+
+    return new URL(oldPathname, "https://circleci.com").toString();
+  }
+
+  const getLocation = () => {
     if (window.location.host.startsWith("circleci.com")) {
-      window.location =
-        "https://app.circleci.com" + toNewPath(window.location.pathname);
-      return;
+      return getNewLocation();
     }
 
-    window.location =
-      "https://circleci.com" + toOldPath(window.location.pathname);
+    return getOldLocation();
+  }
+
+  window.hotkeys("command+j", () => {
+    const otherLocation = getLocation();
+    if (!otherLocation) {
+      console.warn("Could not find matching other location.")
+      return;
+    }
+    window.location = getLocation();
   });
 })();
