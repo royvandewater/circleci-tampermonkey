7353cbe0858a616cd6195b512179ba7478dedb33
Roy van de Water <roy.vwater@gmail.com>
2021-04-09 11:26:15 -0700

    Fix wait-for-optmizely updates

--- wait-for-optimizely-updates.user.js
+++ wait-for-optimizely-updates.user.js
@@ -1,15 +1,13 @@
 // ==UserScript==
 // @name         Wait for Optimizely Updates
 // @namespace    http://royvandewater.com/
-// @version      1.0
-// @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/l-goes-to-latest-workflow.user.js
+// @version      1.1
+// @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/wait-for-optimizely-updates.user.js
 // @description  Will let you know when a new version of Optimizely goes out when looking at the datafile
 // @author       Roy van de Water
-// @match        https://app.circleci.com/api/datafiles
+// @match        https://app.circleci.com/api/datafiles/*
 // @grant        none
 // ==/UserScript==
-
-const { setTimeout } = require("globalthis/implementation");
 
 (function () {
   "use strict";
@@ -64,42 +62,10 @@ const { setTimeout } = require("globalthis/implementation");
       const message = buildNewRevisionMessage()
       document.body.prepend(message);
       document.addEventListener('click', () => message.remove());
+      requestAnimationFrame(() => { waitingMessage.style.top = 0 });
     }
 
     previousVersion = latestVersion
     setTimeout(newRevisionCheck, 1000)
   }
-
-  const getWorkflowId = () => {
-    const parts = window.location.pathname.split('/')
-    const index = parts.indexOf('workflows')
-    return parts[index + 1]
-  }
-
-  window.hotkeys("l", async () => {
-    if (!window.location.pathname.includes("/workflows/")) return;
-
-    const workflowId = getWorkflowId();
-
-    if (!workflowId) return;
-
-    const waitingMessage = buildWaitingMessage();
-    const a = await getBranchA();
-    a.click();
-
-    const click = el => {
-      if (!el) return;
-      if (el.href.includes(workflowId)) return;
-      waitingMessage.remove()
-      document.unbindArrive(click);
-      el.click();
-    };
-
-    click(document.querySelector(workflowASelector))
-    document.arrive(workflowASelector, click);
-    document.body.prepend(waitingMessage);
-    document.addEventListener('click', () => waitingMessage.remove());
-
-    requestAnimationFrame(() => { waitingMessage.style.top = 0 });
-  });
 })();
