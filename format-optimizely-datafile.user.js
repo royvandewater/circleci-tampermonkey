d1a47c40f84d0cd2288bcbc6eba5ba986d8f9236
Roy van de Water <roy@circleci.com>
2022-02-14 09:50:38 -0700

    Add another URL to the optimizely helpers

--- format-optimizely-datafile.user.js
+++ format-optimizely-datafile.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Format Optimizely datafile
 // @namespace    http://royvandewater.com/
-// @version      1.1
+// @version      1.2
 // @updateURL    https://github.com/royvandewater/circleci-tampermonkey/raw/master/format-optimizely-datafile.user.js
 // @description  Pretty prints the datafile
 // @author       Roy van de Water
 // @match        https://app.circleci.com/api/datafiles/*
 // @match        https://app.circleci.com/private/experiments/datafiles/*
+// @match        https://assets-growth-common.circleci.com/optimizely/datafiles/*
 // @grant        none
 // ==/UserScript==
 
